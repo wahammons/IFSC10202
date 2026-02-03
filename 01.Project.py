81c6ca189759560912dd4367556113523f981a2e
wahammons <wahammons@ualr.edu>
2026-02-03 23:01:22 +0000

    Still need to fix

---
+++ 01.Project.py
@@ -0,0 +1,20 @@
+# Whitney Hammons
+
+# AI Usage: None
+
+# Number of years in the length of time
+# Number of days in the length of time
+# Number of hours in the length of time
+# Number of minutes in the length of time
+# Number of seconds in the length of time
+
+time = int(input("Enter length of time in seconds: "))
+day = time * 365
+hour = day * 24
+min = hour * 60
+sec = min * 60
+print(f"Years: {time}")
+print(f"Days: {day}")
+print(f"Hours: {hour}")
+print(f"Minutes: {min}")
+print(f"Seconds: {sec}")
