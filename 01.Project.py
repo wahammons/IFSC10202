a329b48f8fedabd5dd56cdab5948180e80a0ee37
wahammons <wahammons@ualr.edu>
2026-02-04 16:26:34 +0000

    Update

--- 01.Project.py
+++ 01.Project.py
@@ -8,13 +8,20 @@
 # Number of minutes in the length of time
 # Number of seconds in the length of time
 
-time = int(input("Enter length of time in seconds: "))
-day = time * 365
-hour = day * 24
-min = hour * 60
-sec = min * 60
-print(f"Years: {time}")
+total = int(input("Enter length of time in seconds: "))
+m = 60
+h = 60* 60
+d = 24 * 60 * 60
+y = 365 * 24 * 60 * 60
+year = total // y
+left = total % y
+day = left // d
+
+hour = left // h
+
+minute = left // m
+print(f"Years: {year}")
 print(f"Days: {day}")
 print(f"Hours: {hour}")
-print(f"Minutes: {min}")
+print(f"Minutes: {minute}")
 print(f"Seconds: {sec}")
---
+++ Test.py
@@ -0,0 +1,4 @@
+day = time * 365
+hour = day * 24
+minute = hour * 60
+sec = minute * 60
