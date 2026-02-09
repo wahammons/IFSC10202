d59736374575efb0f06265cb9bfe579ba0ebba5b
wahammons <wahammons@ualr.edu>
2026-02-09 16:55:16 +0000

    update

--- 03.Project.py
+++ 03.Project.py
@@ -1,4 +1,19 @@
+
 num1 = float(input("Enter first number: "))
-op = input("Eneter operator: ")
+op = input("Enter operator: ")
 num2 = float(input("Enter second number: "))
-print("num1 op num2")
+add = num1 + num2 
+sub = num1 - num2
+mult = num1 * num2
+div = num1 / num2
+if op == "+":
+    print(num1, "+", num2,"=", add, num1, num2)
+elif op == "-":
+    print(num1, "-", num2,"=", sub, num1, num2)
+elif op == "*":
+    print(num1, "*", num2,"=", mult ,num1, num2)
+elif op == "/":
+    print(num1, "/", num2,"=", div, num1, num2)
+
+total = 
+print(num1, op, num2 "=" )
