c5f8d3e2f6c2ab1ef17215dd20b80cb65bb3c5f1
wahammons <wahammons@ualr.edu>
2026-02-11 14:35:02 +0000

    Started

--- 03.Project.py
+++ 03.Project.py
@@ -7,12 +7,15 @@ sub = num1 - num2
 mult = num1 * num2
 div = num1 / num2
 if op == "+":
-    print(num1, "+", num2,"=", add, num1, num2)
+    print(num1, "+", num2,"=", add)
 elif op == "-":
-    print(num1, "-", num2,"=", sub, num1, num2)
+    print(num1, "-", num2,"=", sub)
 elif op == "*":
-    print(num1, "*", num2,"=", mult ,num1, num2)
+    print(num1, "*", num2,"=", mult)
 elif op == "/":
-    print(num1, "/", num2,"=", div, num1, num2)
+    #move div here and check before if num2 is zero
+    print(num1, "/", num2,"=", div,)
+else:
+    print("Invalid Operator")
 
 
