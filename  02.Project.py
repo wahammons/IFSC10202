59c0e37d140e947c595a2979b5c6e9216cb40c0a
wahammons <wahammons@ualr.edu>
2026-02-11 14:41:35 +0000

    Needs math

---  02.Project.py
+++  02.Project.py
@@ -1,2 +1,9 @@
-from math import pi, sin, cos, arcos, radians
- 
+from math import pi, sin, cos, arccos, radians
+
+print("Great Circle Calculator")
+int(input("Enter Radius of Sphere: "))
+print("Starting Point - Enter Latitude: ")
+print("Starting Point - Enter Longitude: ")
+print("Ending Point - Enter Latitude: ")
+print("Ending Point - Enter Longitude: ")
+print("The Great Circle Distance is ")
