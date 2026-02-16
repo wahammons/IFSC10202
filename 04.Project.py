403c6ab31ab3a3f2eb7514571c8aca795f40f2ba
wahammons <wahammons@ualr.edu>
2026-02-16 16:09:16 +0000

    Done

--- 04.Project.py
+++ 04.Project.py
@@ -1,6 +1,7 @@
 def print_asterisk(height):
     for i in range(1, height + 1):
         print(*"*" * i)
-        
+    for i in range(height - 1, 0, -1):
+        print(*"*" * i)   
 max_height = int(input("Enter maximum height: "))
 print_asterisk(max_height)
