fa1ee801180667dc1dd95654dfdecb852afd5503
wahammons <wahammons@ualr.edu>
2026-02-16 14:33:08 +0000

    started

---
+++ 04.Projoect.py
@@ -0,0 +1,8 @@
+def print_asterisk(height):
+    
+  
+    
+    for i in range(1, height + 1):
+        print(' ' * (height - i) + '*' * i)
+max_height = int(input("Enter maximum height: "))
+print_asterisk(max_height)
