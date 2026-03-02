c9e378c40feabbddc000a77084ddfc164633d4ca
wahammons <wahammons@ualr.edu>
2026-03-02 14:42:15 +0000

    Close

--- 06.Project.py
+++ 06.Project.py
@@ -1,2 +1,14 @@
-outputfile = open("06.Project Output File.txt")
-x = outputfile.readline()
+inputfilename = "06.Project Input File.txt"
+outputfilename = "06.Project Output File.txt"
+inputfile = open(inputfilename, 'r')
+outputfile = open(outputfilename, 'w')
+recordcount = 0
+line = inputfile.readline()
+while line != '':
+    outputfile.write(line)
+    recordcount += 1
+    line = inputfile.readline()
+recordcount = 0
+inputfile.close()
+outputfile.close()
+print("{} records written".format(recordcount))
