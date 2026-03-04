0fc97032d5cf6d0a1cb724f18c7db2bf07015dd3
wahammons <wahammons@ualr.edu>
2026-03-04 10:35:02 -0600

    Enhance file processing with merge functionality
    Updated file processing to include merging from a specified merge file. Added counters for input, merge, and output records.

--- 06.Project.py
+++ 06.Project.py
@@ -1,14 +1,31 @@
 inputfilename = "06.Project Input File.txt"
+mergefilename = "06.Project Merge File.txt"
 outputfilename = "06.Project Output File.txt"
-inputfile = open(inputfilename, 'r')
-outputfile = open(outputfilename, 'w')
-recordcount = 0
-line = inputfile.readline()
-while line != '':
-    outputfile.write(line)
-    recordcount += 1
-    line = inputfile.readline()
-recordcount = 0
-inputfile.close()
-outputfile.close()
-print("{} records written".format(recordcount))
+inputcount = 0
+mergecount = 0
+outputcount = 0
+infile = open(inputfilename, 'r')
+outfile = open(outputfilename, 'w')
+line = infile.readline()
+while line !="":
+     if line == '**Insert Merge File Here**\n':
+          mergefile = open(mergefilename, 'r')
+          mergeLine = mergefile.readline()
+          while mergeLine != '':
+               outfile.write(mergeLine)
+               mergecount += 1
+               outputcount += 1
+               mergeLine = mergefile.readline()
+          mergefile.close()
+     else:
+          outfile.write(line)
+          inputcount += 1
+          outputcount += 1
+
+     line = infile.readline()
+
+infile.close()
+outfile.close()
+print(f"{inputcount} input file records")
+print(f"{mergecount} merge file records")
+print(f"{outputcount} output file records")
